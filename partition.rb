7bb41c756fa4caf7392f7d0c32622361ca4b0df2
Joseph Siefers <joseph.siefers@gmail.com>
2017-01-09 18:07:23 -0500

    Fix errors in partition.

--- partition.rb
+++ partition.rb
@@ -16,14 +16,14 @@ def partition(ar, i, j)
 			right=right-1
 		elsif ar[right] > pivot_item
 			right=right-1
-		elsif ar[left] < pivot_item
-			#swap is not possible, so move index inward
+		elsif ar[left] <= pivot_item
 			left=left+1
 		end
 	end
 
-	#swap the pivot item into its final location
-	swap(ar, i, right)
+	#swap the pivot item into its final location, unless no swaps occured previously (everything must be greater or less)
+	swap(ar, i, right) unless ar[i] < ar[right]
+	ar
 end
 
 def swap(ar, i, j)
@@ -44,15 +44,15 @@ def it_partitions_an_array_of_one
 end
 
 def it_partitions_an_array_of_two
-	ar = [1, 0]
+	ar = [0, 1]
 
 	assert(partition(ar, 0, (ar.length-1)), [0, 1])
 end
 
 def it_partitions_an_array_of_three
-	ar = [8, 3, 1]
+	ar = [1, 0, 2]
 
-	assert(partition(ar, 0, (ar.length-1)), [1, 3, 8])
+	assert(partition(ar, 0, (ar.length-1)), [0, 1, 2])
 end
 
 def it_partitions_a_more_complicated_example
@@ -61,9 +61,30 @@ def it_partitions_a_more_complicated_example
 	assert(partition(ar, 0, (ar.length-1)), [3, 2, 1, 5, 7, 9, 8])
 end
 
+def it_partitions_with_everything_greater
+	ar = [0, 9, 1, 1, 2, 3, 4, 5]
+
+	assert(partition(ar, 0, (ar.length-1)), [0, 9, 1, 1, 2, 3, 4, 5])
+end
+
+def it_partitions_with_everything_less
+	ar = [9, 1, 1, 5, 2, 3, 4, 1]
+
+	assert(partition(ar, 0, (ar.length-1)), [1, 1, 1, 5, 2, 3, 4, 9])
+end
+
+def it_partitions_a_parasitic_example
+	ar = [1, 1, 1, 1, 1, 1, 1]
+
+	assert(partition(ar, 0, (ar.length-1)), [1, 1, 1, 1, 1, 1, 1])
+end
+
 it_partitions_an_array_of_one()
 it_partitions_an_array_of_two()
 it_partitions_an_array_of_three()
 it_partitions_a_more_complicated_example()
+it_partitions_with_everything_greater()
+it_partitions_with_everything_less()
+it_partitions_a_parasitic_example
 
 puts "Passed"
