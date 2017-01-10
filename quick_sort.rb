4b72703dc068de9fb57ea341323c61b48287fb7a
Joseph Siefers <joseph.siefers@gmail.com>
2017-01-10 13:21:47 -0500

    Fix quick sort (again).

--- quick_sort.rb
+++ quick_sort.rb
@@ -6,14 +6,13 @@ def quick_sort(ar, i=0, j=nil)
 
 	return ar if i >= j
 
-	#puts "Partitioning: (#{i},#{j}) - #{ar.join(' ')}"
-	midpoint = partition(ar, i, j)
-	#puts "After partition: #{ar.join(' ')}"
-	puts ar.join(' ')
-
-	#puts "midpoint: #{midpoint}"
-	quick_sort(ar, i, midpoint-1)
-	quick_sort(ar, (midpoint+1), j)
+	#puts "Partitioning: (#{i},#{j})"
+	pivot_idx = partition(ar, i, j)
+	#puts ar.join(', ')
+	#puts "pivot_idx: #{pivot_idx} pivot_item: #{ar[pivot_idx]}"
+	#check_partition(ar, pivot_idx)
+	quick_sort(ar, i, pivot_idx-1)
+	quick_sort(ar, (pivot_idx+1), j)
 	ar
 end
 
@@ -31,23 +30,38 @@ def partition(ar, i, j)
 		#puts "left: #{left} #{ar[left]} right: #{right} #{ar[right]}"
 		if (ar[left] > pivot_item && ar[right] < pivot_item)
 			#swap is possible (mutually benefitial)
+			#puts "Swapping left and right"
 			swap(ar, left, right)
 			left=left+1
 			right=right-1
 		elsif ar[left] < pivot_item
+			#puts "Advancing left"
 			left=left+1
 		elsif ar[right] >= pivot_item
+			#puts "Advancing right"
 			right=right-1
 		end
-		#puts ar.join(' ')
 	end
 
-	#puts "Final swap: #{ar[pivot]} #{ar[right]}"
-	if ar[pivot] > ar[left]
-		return pivot
+	#puts "Left: #{left}, #{ar[left]} pivot: #{pivot} #{ar[pivot]}"
+	if (ar[left] < ar[pivot])
+		swap(ar, pivot, left+1)
+		return (left + 1)
 	else
 		swap(ar, pivot, left)
 		return left
+	end
+end
+
+def check_partition(ar, pivot_idx)
+	pivot_item = ar[pivot_idx]
+
+	(0..(pivot_idx-1)).each do |i|
+		raise "#{pivot_idx}: #{pivot_item} < #{i}: #{ar[i]}" if pivot_item < ar[i]
+	end
+
+	((pivot_idx+1)..(ar.length-1)).each do |i|
+		raise "#{pivot_idx}: #{pivot_item} < #{i}: #{ar[i]}" if pivot_item > ar[i]
 	end
 end
 
@@ -104,6 +118,14 @@ def it_sorts_a_parasitic_example
 	assert(quick_sort(ar), [1, 1, 1, 1, 1, 1])
 end
 
+def it_sorts_a_fucking_ridiculous_example
+	ar = [406, 157, 415, 318, 472, 46, 252, 187, 364, 481, 450, 90, 390, 35, 452, 74, 196, 312, 142, 160, 143, 220, 483, 392, 443, 488, 79, 234, 68, 150, 356, 496, 69, 88, 177, 12, 288, 120, 222, 270, 441, 422, 103, 321, 65, 316, 448, 331, 117, 183, 184, 128, 323, 141, 467, 31, 172, 48, 95, 359, 239, 209, 398, 99, 440, 171, 86, 233, 293, 162, 121, 61, 317, 52, 54, 273, 30, 226, 421, 64, 204, 444, 418, 275, 263, 108, 10, 149, 497, 20, 97, 136, 139, 200, 266, 238, 493, 22, 17, 39]
+	
+	assert(quick_sort(ar), [10, 12, 17, 20, 22, 30, 31, 35, 39, 46, 48, 52, 54, 61, 64, 65, 68, 69, 74, 79, 86, 88, 90, 95, 97, 99, 103, 108, 117, 120, 121, 128, 136, 139, 141, 142, 143, 149, 150, 157, 160, 162, 171, 172, 177, 183, 184, 187, 196, 200, 204, 209, 220, 222, 226, 233, 234, 238, 239, 252, 263, 266, 270, 273, 275, 288, 293, 312, 316, 317, 318, 321, 323, 331, 356, 359, 364, 390, 392, 398, 406, 415, 418, 421, 422, 440, 441, 443, 444, 448, 450, 452, 467, 472, 481, 483, 488, 493, 496, 497])
+
+
+end
+
 it_sorts_an_array_with_one_element()
 it_sorts_an_array_with_two_elements()
 it_sorts_an_array_with_three_elements()
@@ -111,6 +133,7 @@ it_sorts_a_more_complicated_example()
 it_sorts_another_complicated_example()
 it_sorts_a_sorted_array()
 it_sorts_a_parasitic_example()
+it_sorts_a_fucking_ridiculous_example()
 
 #partition(4,6)
 #0 1 2 3 4 5 6
